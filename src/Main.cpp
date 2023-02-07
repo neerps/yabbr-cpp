6affa68f39005c4cf8a9275a5fe00235a528346a
neerps <c4se.dust@gmail.com>
2023-02-07 22:31:00 +0300

    Update main
    Change to have a scene with a gradient.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 /*
 */
 #include "Color.h"
+#include "Ray.h"
 #include "Vec3.h"
 
 #include <iostream>
@@ -9,8 +10,19 @@
 int main()
 {
   // Image
-  const int imageWidth{256};
-  const int imageHeight{256};
+  const auto aspectRatio{16.0 / 9.0};
+  const int imageWidth{400};
+  const int imageHeight{static_cast<int>(imageWidth / aspectRatio)};
+
+  // Camera
+  auto viewportHeight{2.0};
+  auto viewportWidth{aspectRatio * viewportHeight};
+  auto focalLength{1.0};
+
+  auto origin{Point3{0, 0, 0}};
+  auto horizontal{Vec3{viewportWidth, 0, 0}};
+  auto vertical{Vec3{0, viewportHeight, 0}};
+  auto lowerLeftCorner{origin - horizontal / 2 - vertical / 2 - Vec3{0, 0, focalLength}};
 
   // Render
   std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
@@ -20,7 +32,10 @@ int main()
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i{0}; i < imageWidth; ++i)
     {
-      Color pixelColor{static_cast<double>(i) / (imageWidth - 1), static_cast<double>(j) / (imageHeight - 1), 0.25};
+      auto u{static_cast<double>(i) / (imageWidth - 1)};
+      auto v{static_cast<double>(i) / (imageHeight - 1)};
+      Ray r{origin, lowerLeftCorner + u * horizontal + v * vertical - origin};
+      Color pixelColor{rayColor(r)};
       writeColor(std::cout, pixelColor);
     }
   }
