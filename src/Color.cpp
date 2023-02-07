740f81469ad6a7b9f7783b6bfc920ae7b0d89bd0
neerps <c4se.dust@gmail.com>
2023-02-07 22:30:34 +0300

    Update utility functions for Ray

--- src/Color.cpp
+++ src/Color.cpp
@@ -2,6 +2,7 @@
 */
 
 #include "Color.h"
+#include "Ray.h"
 #include "Vec3.h"
 
 #include <iostream>
@@ -12,4 +13,12 @@ void writeColor(std::ostream& out, const Color& pixelColor)
   out << static_cast<int>(255.999 * pixelColor.x()) << ' '
     << static_cast<int>(255.999 * pixelColor.y()) << ' '
     << static_cast<int>(255.999 * pixelColor.z()) << '\n';
+}
+
+//
+Color rayColor(const Ray& r)
+{
+  Vec3 unitDirection{unitVector(r.direction())};
+  auto t{0.5 * (unitDirection.y() + 1.0)};
+  return (1.0 - t) * Color{1.0, 1.0, 1.0} + t * Color{0.5, 0.7, 1.0};
 }
--- src/Color.h
+++ src/Color.h
@@ -1,11 +1,15 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "Ray.h"
 #include "Vec3.h"
 
 #include <iostream>
 
 //
 void writeColor(std::ostream& out, const Color& pixelColor);
+
+//
+Color rayColor(const Ray& r);
 
 #endif
