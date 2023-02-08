2ca635ce74fb33e42b607fb4d70e0b77ffc41b2c
neerps <c4se.dust@gmail.com>
2023-02-08 21:55:59 +0300

    Add notion of a side of a normal to Sphere object

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -36,6 +36,7 @@ bool Sphere::hit(const Ray& r, double tMin, double tMax, HitRecord& rec) const
   rec.t = root;
   rec.p = r.at(rec.t);
   Vec3 outwardNormal{(rec.p - m_center) / m_radius};
+  rec.setFaceNormal(r, outwardNormal);
 
   return true;
 }
