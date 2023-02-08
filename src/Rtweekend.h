5721483bc26bdce6a5b57e887641255c1b0f4470
neerps <c4se.dust@gmail.com>
2023-02-08 22:13:10 +0300

    Add common header

---
+++ src/Rtweekend.h
@@ -0,0 +1,26 @@
+/*
+*/
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+// Common headers
+#include "Ray.h"
+#include "Vec3.h"
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+// Aliases
+
+// Constants
+inline constexpr double infinity{std::numeric_limits<double>::infinity()};
+inline constexpr double pi{3.1415926535897932385};
+
+// Utility Functions
+inline double degreesToRadians(double degrees)
+{
+  return degrees * pi / 180.0;
+}
+
+#endif
