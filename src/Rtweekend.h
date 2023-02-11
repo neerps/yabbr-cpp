fb96ed1940acbe347effe6b7fbadfb6676cdb45f
neerps <c4se.dust@gmail.com>
2023-02-11 21:37:12 +0300

    Add clamping function

--- src/Rtweekend.h
+++ src/Rtweekend.h
@@ -23,4 +23,13 @@ inline double degreesToRadians(double degrees)
   return degrees * pi / 180.0;
 }
 
+inline double clamp(double x, double min, double max)
+{
+  if (x < min)
+    return min;
+  if (x > max)
+    return max;
+  return x;
+}
+
 #endif
