431c6594b69e631817e81ef48630c0b954b7c50d
neerps <c4se.dust@gmail.com>
2023-02-11 21:53:18 +0300

    Update main for changes

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,9 @@
 /*
 */
+#include "Camera.h"
 #include "Color.h"
 #include "HittableList.h"
+#include "RandomDoubleGen.h"
 #include "Rtweekend.h"
 #include "Sphere.h"
 
@@ -10,10 +12,14 @@
 //
 int main()
 {
+  //
+  RandomDoubleGen rng{};
+
   // Image
   const auto aspectRatio{16.0 / 9.0};
   const int imageWidth{400};
   const int imageHeight{static_cast<int>(imageWidth / aspectRatio)};
+  const int samplesPerPixel{100};
 
   // World
   HittableList world;
@@ -21,14 +27,7 @@ int main()
   world.add(std::make_shared<Sphere>(Point3{0, -100.5, -1}, 100));
 
   // Camera
-  auto viewportHeight{2.0};
-  auto viewportWidth{aspectRatio * viewportHeight};
-  auto focalLength{1.0};
-
-  auto origin{Point3{0, 0, 0}};
-  auto horizontal{Vec3{viewportWidth, 0, 0}};
-  auto vertical{Vec3{0, viewportHeight, 0}};
-  auto lowerLeftCorner{origin - horizontal / 2 - vertical / 2 - Vec3{0, 0, focalLength}};
+  Camera cam{};
 
   // Render
   std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
@@ -38,11 +37,15 @@ int main()
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i{0}; i < imageWidth; ++i)
     {
-      auto u{static_cast<double>(i) / (imageWidth - 1)};
-      auto v{static_cast<double>(j) / (imageHeight - 1)};
-      Ray r{origin, lowerLeftCorner + u * horizontal + v * vertical - origin};
-      Color pixelColor{rayColor(r, world)};
-      writeColor(std::cout, pixelColor);
+      Color pixelColor{0, 0, 0};
+      for (int s{0}; s < samplesPerPixel; ++s)
+      {
+        auto u{(i + rng.getRandom(0, 1)) / (imageWidth - 1)};
+        auto v{(j + rng.getRandom(0, 1)) / (imageHeight - 1)};
+        Ray r{cam.getRay(u, v)};
+        pixelColor += rayColor(r, world);
+      }
+      writeColor(std::cout, pixelColor, samplesPerPixel);
     }
   }
   std::cerr << "\nDone.\n";
