eba0c8096edbdcde8f910d23d3b1c3f2b5820d63
neerps <c4se.dust@gmail.com>
2023-02-20 21:22:39 +0300

    Add virtual destructor

--- src/Material.h
+++ src/Material.h
@@ -12,6 +12,7 @@ struct HitRecord;
 class Material
 {
 public:
+  virtual ~Material() = default;
   virtual bool scatter(
     const Ray& rIn, const HitRecord& rec, const RandomGen& rng, Color& attenuation, Ray& scattered
   ) const = 0;
