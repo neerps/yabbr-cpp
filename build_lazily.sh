ba4d28a7f04839199cea98bd5b6f41e685af4c2c
neerps <c4se.dust@gmail.com>
2023-03-08 18:21:13 +0300

    Add simple build script

---
+++ build_lazily.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+src_dir=src
+artifacts_dir=results
+exec_name=Main
+
+srcs=()
+while IFS=  read -r -d $'\0' ; do
+  srcs+=("$REPLY")
+done < <(find "$src_dir" -name "*.cpp" -print0)
+
+/usr/bin/g++ -o2 -pedantic-errors -Wall -Weffc++ -Wextra -Wsign-conversion -Werror -o "${artifacts_dir}"/"${exec_name}" "${srcs[@]}"
+
+tag_to_print=$(git describe --tags --long)
+commit_info="$(date +%Y-%m-%d)"."$tag_to_print"
+image_name=color_debug."$commit_info".ppm
+
+"${artifacts_dir}"/"${exec_name}" > "${artifacts_dir}"/"${image_name}"
+
+display "${artifacts_dir}"/"${image_name}"
