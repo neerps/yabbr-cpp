ed91369cfcc820129fa34a4f18ea0aedba45c2f7
neerps <c4se.dust@gmail.com>
2023-03-08 21:25:44 +0300

    Change refraction

--- src/Dielectric.cpp
+++ src/Dielectric.cpp
@@ -15,9 +15,18 @@ ScatterResult Dielectric::scatter(const Ray& rIn, const HitRecord& rec) const
   double refractionRatio{rec.frontFace ? (1.0 / m_ir) : m_ir};
 
   Vec3 unitDirection{unitVector(rIn.direction())};
-  Vec3 refracted{refract(unitDirection, rec.normal, refractionRatio)};
+  double cosTheta{fmin(dot(-unitDirection, rec.normal), 1.0)};
+  double sinTheta{std::sqrt(1.0 - cosTheta * cosTheta)};
 
-  res.scattered = Ray{rec.p, refracted};
+  bool cannotRefract{refractionRatio * sinTheta > 1.0};
+  Vec3 direction{};
+
+  if (cannotRefract)
+    direction = reflect(unitDirection, rec.normal);
+  else
+    direction = refract(unitDirection, rec.normal, refractionRatio);
+
+  res.scattered = Ray{rec.p, direction};
   res.isScattered = true;
 
   return res;
--- src/Main.cpp
+++ src/Main.cpp
@@ -27,9 +27,9 @@ int main()
   HittableList world{};
 
   auto materialGround{std::make_shared<Lambertian>(Color{0.8, 0.8, 0.0})};
-  auto materialCenter{std::make_shared<Dielectric>(1.5)};
+  auto materialCenter{std::make_shared<Lambertian>(Color{0.1, 0.2, 0.5})};
   auto materialLeft{std::make_shared<Dielectric>(1.5)};
-  auto materialRight{std::make_shared<Metal>(Color{0.8, 0.6, 0.2}, 1.0)};
+  auto materialRight{std::make_shared<Metal>(Color{0.8, 0.6, 0.2}, 0.0)};
 
   world.add(std::make_shared<Sphere>(Point3{0.0, -100.5, -1.0}, 100.0, materialGround));
   world.add(std::make_shared<Sphere>(Point3{0.0, 0.0, -1.0}, 0.5, materialCenter));
