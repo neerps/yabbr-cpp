979a6bf58c287708b0adc8cfd1f212ab12dd6388
neerps <c4se.dust@gmail.com>
2023-03-08 18:27:54 +0300

    Fix variable style

--- build_lazily.sh
+++ build_lazily.sh
@@ -6,15 +6,15 @@ exec_name=Main
 
 srcs=()
 while IFS=  read -r -d $'\0' ; do
-  srcs+=("$REPLY")
-done < <(find "$src_dir" -name "*.cpp" -print0)
+  srcs+=("${REPLY}")
+done < <(find "${src_dir}" -name "*.cpp" -print0)
 
 /usr/bin/g++ -o2 -pedantic-errors -Wall -Weffc++ -Wextra -Wsign-conversion -Werror -o "${artifacts_dir}"/"${exec_name}" "${srcs[@]}"
 
 branch_to_print=$(git rev-parse --abbrev-ref HEAD)
 tag_to_print=$(git describe --tags --long)
-commit_info="$(date +%Y-%m-%d)"."$tag_to_print"
-image_name="$branch_to_print"."$commit_info".ppm
+commit_info="$(date +%Y-%m-%d)"."${tag_to_print}"
+image_name="${branch_to_print}"."${commit_info}".ppm
 
 "${artifacts_dir}"/"${exec_name}" > "${artifacts_dir}"/"${image_name}"
 
