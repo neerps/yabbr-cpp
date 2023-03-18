70a82dcd3adb67f0919cec26765aeb96d91576c3
neerps <c4se.dust@gmail.com>
2023-03-18 22:00:21 +0300

    Update shell script
    Update for use with Makefile

--- build_lazily.sh
+++ build_lazily.sh
@@ -2,28 +2,19 @@
 
 src_dir=src
 artifacts_dir=results
-exec_name=Main
+build_dir=build
+exec_name=raytracing_one
 
-srcs=()
-while IFS=  read -r -d $'\0' ; do
-  srcs+=("${REPLY}")
-done < <(find "${src_dir}" -name "*.cpp" -print0)
+make_opts=(SRC_DIR="${src_dir}" BUILD_DIR="${build_dir}" EXEC_NAME="${exec_name}")
 
-/usr/bin/g++\
-  -o2 -pedantic-errors\
-  -Wall -Weffc++\
-  -Wextra\
-  -Wsign-conversion\
-  -Werror\
-  -o "${artifacts_dir}"/"${exec_name}"\
-  "${srcs[@]}" \
-|| exit 1
+make clean "${make_opts[@]}"
+make "${make_opts[@]}"
 
 branch_to_print=$(git rev-parse --abbrev-ref HEAD)
 tag_to_print=$(git describe --tags --long)
 commit_info="$(date +%Y-%m-%d)"."${tag_to_print}"
 image_name="${branch_to_print}"."${commit_info}".ppm
 
-"${artifacts_dir}"/"${exec_name}" > "${artifacts_dir}"/"${image_name}"
+"${build_dir}"/"${exec_name}" > "${artifacts_dir}"/"${image_name}"
 
 display "${artifacts_dir}"/"${image_name}"
