1f6656fccdb4e758b5ec10c6e0d6173093c148c5
neerps <c4se.dust@gmail.com>
2023-03-18 22:08:27 +0300

    Update image name

--- build_lazily.sh
+++ build_lazily.sh
@@ -10,10 +10,12 @@ make_opts=(SRC_DIR="${src_dir}" BUILD_DIR="${build_dir}" EXEC_NAME="${exec_name}
 make clean "${make_opts[@]}"
 make "${make_opts[@]}"
 
+printed_sha_length=8
 branch_to_print=$(git rev-parse --abbrev-ref HEAD)
-tag_to_print=$(git describe --tags --long)
-commit_info="$(date +%Y-%m-%d)"."${tag_to_print}"
-image_name="${branch_to_print}"."${commit_info}".ppm
+date_to_print="$(date +%Y-%m-%d)"
+commit_info="$(git describe --exact-match --tags 2>/dev/null\
+  || git rev-parse --short="$printed_sha_length" HEAD)"
+image_name="${date_to_print}"."${branch_to_print}"."${commit_info}".ppm
 
 "${build_dir}"/"${exec_name}" > "${artifacts_dir}"/"${image_name}"
 
